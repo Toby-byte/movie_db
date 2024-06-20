56e7dc9e892a309573320d3554dd572a4d9a863a
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-06-20 12:03:27 +0200

    movie db v1 complete

--- js/series.js
+++ js/series.js
@@ -13,19 +13,31 @@ fetch('json/series.json').then(response => {
         const series = data.series
         // For each movie create a 'p' element and add it to the movie section in the HTML file
         series.forEach(serie => {
+            // Create the series title element
             seriesElement = document.createElement('h1');
             seriesElement.textContent = `${serie.title}`;
+
+            // Create the big container for series and heading
+            const BigContainerSeriesAndHeading = document.createElement('article');
+            BigContainerSeriesAndHeading.classList.add("BigContainerSeriesAndHeading");
             
-            seriesContainer = document.createElement('article');
+            // Create the container for the series image
+            const seriesContainer = document.createElement('article');
             seriesContainer.classList.add("seriesContainer");
 
-            seriesImg = document.createElement('img');
-            seriesImg.src = "poster.webp";
+            // Create the image element
+            const seriesImg = document.createElement('img');
+            seriesImg.src = `series_pictures/${serie.title}.webp`;
 
+            // Append the image to the series container
             seriesContainer.appendChild(seriesImg);
-            seriesContainer.appendChild(seriesElement)
-            series_section.appendChild(seriesContainer);
-            
+
+            // Append the series container and title element to the big container
+            BigContainerSeriesAndHeading.appendChild(seriesContainer);
+            BigContainerSeriesAndHeading.appendChild(seriesElement);
+
+            // Append the big container to the series section
+            series_section.appendChild(BigContainerSeriesAndHeading);
         });
 })
 // catch any errors that could occur, fecthing the movies from the json file
