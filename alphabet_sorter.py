d9c9c50641920519a49691c420ad5b86545c728e
Tobias Bøge Nielsen <cybertoby2349@gmail.com>
2024-09-29 17:50:02 +0200

    added new movies

--- alphabet_sorter.py
+++ alphabet_sorter.py
@@ -19,8 +19,8 @@ def sort_csv_alphabetically(input_file, output_file):
         writer.writerows(sorted_rows)
 
 # Example usage
-input_csv = 'csv\series.csv'
-output_csv = 'csv\sorted_series.csv'
+input_csv = 'csv\movies.csv'
+output_csv = 'csv\sorted_movies.csv'
 sort_csv_alphabetically(input_csv, output_csv)
 
 print(f"Sorted CSV file has been saved as {output_csv}")
